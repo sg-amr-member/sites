7455a8124a124a988083791e0fbce068fbede19e
sg-amr-member <163960210+sg-amr-member@users.noreply.github.com>
2024-03-20 10:18:08 +0900

    Delete system.js

--- system.js
+++
@@ -1,6 +0,0 @@
-function input_spreadsheets(sheet_id){
-d3.csv("https://docs.google.com/spreadsheets/d/"+
-sheet_id+"/export?format=csv&gid=1948877772", function(error, data){
-	var text = data[1].ert;
-	return(text);
-});};

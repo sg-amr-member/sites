17f6f63c66b449b09f3ba17ae012e767f31895c9
sg-amr-member <163960210+sg-amr-member@users.noreply.github.com>
2024-03-20 10:03:30 +0900

    Update system.js

--- system.js
+++ system.js
@@ -1,10 +1,6 @@
-<!--ここからSpreadSheetの一部を表示する-->
-<script>
 function input_spreadsheets(sheet_id){
 d3.csv("https://docs.google.com/spreadsheets/d/"+
 sheet_id+"/export?format=csv&gid=1948877772", function(error, data){
 	var text = data[1].ert;
 	return(text);
 });};
-</script>
-<!--ここまでSpreadSheetの一部を表示する-->	

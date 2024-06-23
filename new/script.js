5ed9eb1ad4da1a0318241baccd482fb0e2910e16
sonneko <sonneko27@gmail.com>
2024-06-23 09:26:17 +0900

    f

---
+++ new/script.js
@@ -0,0 +1,12 @@
+
+//footerの追加
+const footerElement = document.getElementById("footer");
+footerElement.innerHTML = "<h1>gsg</h1>";
+
+
+
+
+
+
+"<footer>\r\n        <h4>\u30b5\u30a4\u30c8\u30de\u30c3\u30d7<\/h4>\r\n        <a href=\"index.html\">\u30db\u30fc\u30e0<\/a>\r\n        <a href=\"plus.html\">\u5fd9\u3057\u304f\u306a\u308b\u30da\u30fc\u30b8<\/a>\r\n        <a href=\"tool.html\">\u63d0\u4f9b\u3057\u3066\u3044\u308b\u30c4\u30fc\u30eb<\/a>\r\n        <a href=\"study.html\">\u52c9\u5f37\u4f1a\u306e\u304a\u77e5\u3089\u305b<\/a>\r\n    <\/footer>";
+
